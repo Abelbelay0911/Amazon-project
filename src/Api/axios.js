27bc6bb9afd6e14f959b3270e5ae4dda999d156e
abel <abelgebeyehub@gmail.com>
2025-01-06 12:51:14 -0500

    Amazon Project Front-End

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,12 +2,8 @@ import axios from "axios";
 // make only one of the api versions on and comment out the others
 
 const axiosInstance = axios.create({
-  // local instance of firebase function
-  baseURL: "http://127.0.0.1:5001/clone-7fd3d/us-central1/api",
-
-  //Deployed version of amazon server on Render.com
-  //   baseURL: "https://amazon-api-deployed-8coy.onrender.com",
+  // baseURL: "http://127.0.0.1:5001/clone-7fd3d/us-central1/api",
+ baseURL: "https://us-central1-clone-7fd3d.cloudfunctions.net/api",
 });
 
-// Named export to avoid confusion with axios that comes with default export
 export { axiosInstance };
